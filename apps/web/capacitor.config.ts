3bda9c28f9e8acbe1c1824b619c7ef3e7936a34f
Tec94 <63695678+Tec94@users.noreply.github.com>
2025-09-01 15:38:26 -0500

    upd: Capacitor config id for iOS

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.datingornot.app',
+  appId: 'b2c542a8',
   appName: 'Dating or Not',
   webDir: 'dist',
   server: {
